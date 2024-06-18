69bb39d3ae71247e84e31c7a564f71262e78936c
rishavranjan943 <rishavranjan943@gmail.com>
2024-06-19 04:18:53 +0530

    Making base of index and detail page

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    products=Product.objects.all()
+    return render(request,'index.html',{'products':products})
+
+
+def detail(request,id):
+    products=Product.objects.get(id=id)
+    return render(request,'detail.html',{'product':products})
